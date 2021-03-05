7507247675df695f6c848c15910bef34fde119a0
Rootselection <48713457+Rootselection@users.noreply.github.com>
2021-03-05 16:41:29 +0200

    +++

--- js/main.js
+++ js/main.js
@@ -31,12 +31,16 @@ $(window).scroll(function(){
         $(".header__top").css("background","#00B2A0");
         $(".header__top").css("padding-top","0");
         $(".header__top").css("padding-bottom","0");
+        $(".header__top").css("box-shadow","0 0 5px black");
         $(".header__top-inner").css("align-items","center");
+        $(".nav").css("box-shadow","0 0 5px black");
 
        } else if (scrollBottom  > 120) {
         $(".header__top").css("background","transparent");
         $(".header__top").css("padding-top","35px");
         $(".header__top").css("padding-bottom","40px");
+        $(".header__top").css("box-shadow","0 0 0 black");
+        $(".nav").css("box-shadow","0 0 5px black");
       }
     });
 
